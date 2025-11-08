5d06cb339c3367b56b63dae7284c5b92c4ca0ca0
Neversaynever24 <arturkadyrov729@gmail.com>
2025-11-09 02:20:54 +0500

    завершил тестовое задание

--- src/main/java/org/example/test_task/controler/CarController.java
+++ src/main/java/org/example/test_task/controler/CarController.java
@@ -1,11 +1,17 @@
 package org.example.test_task.controler;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.test_task.dto.CarDto;
 import org.example.test_task.dto.PersonWithCarDto;
 import org.example.test_task.service.CarService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
